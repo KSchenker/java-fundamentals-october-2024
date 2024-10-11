d42a8f9ae2c56b85e13379517ee23acec9c2fec6
Max Mustermann <max.mustermann@web.de>
2024-10-11 14:57:42 +0200

    Parsing, CSV-Daten parsen

--- programme/010-Formatierung-und-Parsing/src/ParseDemo.java
+++ programme/010-Formatierung-und-Parsing/src/ParseDemo.java
@@ -1,5 +1,6 @@
 import java.text.NumberFormat;
 import java.text.ParseException;
+import java.util.Arrays;
 import java.util.Locale;
 
 public class ParseDemo {
@@ -31,6 +32,49 @@ public class ParseDemo {
         System.out.printf("%f\n", d);
         int i = n.intValue(); // Wandle die Number in einen int um.
         System.out.println(i);
+
+        String fileContents = """
+                Mustermann;Max;2001-03-12;19.5;m
+                Wonderland ;Alice;1999-02-05;14.25;f
+                "Ban Jonsey";"Bob";1990-01-30;12;m""";
+
+        // Aufgabe: Zerlege den Dateiinhalt in mehrere Datensätze (Zeilen). Trenne die Zeilen wiederum in mehrere
+        // Felder auf (getrennt durch ;). Entferne führende und nachfolgenden Leeraum (Whitespace). Entferne führende und nachfolgende
+        // Hochkommas "". Das Jahr, der Monat und der Tag sollen als Integer geparst werden. Der Steuersatz als double.
+        // Das Geschlecht ist als char zu speichern.
+        String[] lines = fileContents.split("\n");
+        for (String aLine : lines) {
+            String[] fields = aLine.split(";");
+            // Bereinige die Felder
+            for (int index = 0; index < fields.length; index++) {
+                String field = fields[index];
+                // Entferne Leerraum am Anfang und am Ende.
+                field = field.strip();
+                // Entferne ein führendes "
+                if (field.startsWith("\"")) {
+                    field = field.substring(1);
+                }
+                // Entferne ein am Ende stehendes "
+                if (field.endsWith("\"")) {
+                    field = field.substring(0, field.length() - 1);
+                }
+                // Speichere das transformierte im Array zurück.
+                fields[index] = field;
+            }
+            // Trenne das Datumsfeld in seine Bestandteile auf.
+            String[] dateParts = fields[2].split("-");
+            // Wandle die einzelnen Bestandteile in Zahlen um.
+            int year = Integer.parseInt(dateParts[0]);
+            int month = Integer.parseInt(dateParts[1]);
+            int day = Integer.parseInt(dateParts[2]);
+            // Ermittle den Steuersatz als double
+            double taxRate = Double.parseDouble(fields[3]);
+            // Ermittle das Geschlecht als Character
+            char gender = fields[4].charAt(0);
+            // Gib die Daten formatiert aus
+            System.out.printf("Nachname: %-20s Vorname: %8s Geburtsdatum: %02d.%02d.%04d Steuersatz: %.2f Geschlecht: %c\n",
+                    fields[0], fields[1], day, month, year, taxRate, gender);
+        }
 
     }
 
