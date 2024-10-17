e4c753057e7e0e6c60d321fe05f21451a8c805bf
Max Mustermann <max.mustermann@web.de>
2024-10-17 10:45:57 +0200

    Fehlerbehandlung

--- programme/013-Fehlerbehandlung/src/ExceptionDemo.java
+++ programme/013-Fehlerbehandlung/src/ExceptionDemo.java
@@ -1,10 +1,18 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class ExceptionDemo {
 
     public static void main(String[] args) throws IOException {
+        //introduction();
+        //f();
+        System.out.println(readNumberV3("Welches Jahr haben wir", 3));
+    }
+
+    private static void introduction() throws IOException {
         // In Java werden Fehler durch Objekte der Klasse Exception repräsentiert. Man unterscheidet
         // gewöhnliche Exceptions (man muss sie abfangen / checked exceptions) und ungewöhnliche Exceptions
         // (kann man abfangen / unchecked exceptions).
@@ -24,5 +32,138 @@ public class ExceptionDemo {
         //    den Methodennamen eine throws-Klausel ergänzen. Diese Klausel definiert die nicht abgefangenen Exceptions.
         Files.readAllLines(Path.of("words.txt"));
     }
+
+    public static void f() {
+        System.out.println("f startet");
+        g();
+        System.out.println("f endet");
+    }
+
+    public static void g() {
+        System.out.println("g startet");
+        // Die try-Anweisung besteht aus mindestens einem Block. Der erste Block enthält Anweisungen, deren Exceptions
+        // wir behandeln möchten. Danach folgen ggf. mehrere catch-Blöcke. Jeder catch-Block kann sich um eine oder
+        // mehrere Exeptions "kümmern" bzw. diese "behandeln".
+        try {
+            h();
+            // Hier können noch weitere Anweisungen stehen. Diese werden jedoch nur dann ausgeführt, wenn h()
+            // keine Exceptions auslöst.
+        } catch (RuntimeException e) {
+            // Hier erfolgt die Fehlerbehandlung für Exceptions vom Typ RuntimeException. Das schließt auch alle
+            // Exceptions ein, deren Klassen Nachfahren von RuntimeException sind. e ist hier ein frei gewählter
+            // Name für das Exception-Objekt, das ausgelöst bzw. per "throw" delegiert wurde.
+            System.out.println("Beim Aufruf von h ist etwas schief gegangen. Sorry");
+            System.out.println("Die Meldung der Exception lautet: " + e.getMessage());
+            // Wenn wir den Fehler nicht vollständig beheben können, dann geben wir das Exception einfach an unseren
+            // Aufrufer weiter (manuelle Delegation).
+            // Hier gibt es 2 Möglichkeiten: Entweder wir geben das Original-Exception-Objekt e per throw e; weiter
+            // oder wir verpacken e nochmals in eine andere und ggf. abstraktere Exception.
+            throw new RuntimeException("Interner Fehler", e);
+        }
+        // Diese Anweisung wird nur dann ausgeführt, wenn alle Anweisungen im try-Block vollständig ausgeführt wurden
+        // oder wenn die im try-Block ausgelöste Exception von einem catch-Block vollständig behandelt wurde.
+        System.out.println("g endet");
+    }
+
+    public static void h() {
+        System.out.println("h startet");
+        // Exceptions sind Objekte von Klassen, die direkt oder indirekt von Klasse Exception abstammen.
+        var myException = new RuntimeException("Oops! Kernel panic!");
+        // Die Anweisung throw "löst eine Exception aus".
+        throw myException;
+        // System.out.println("h endet"); <-- Diese Anweisung kann nicht mehr ausgeführt werden!
+    }
+
+    public static int readNumber(String prompt, int tries) {
+        // Aufgabe: Gib den Prompt (Eingabeaufforderung) auf der Kommandozeile aus. Lasse den Nutzer anschließend
+        // eine Zahl eingeben. Falls es sich um keine Zahl handelt, soll er weitere Eingabemöglichkeiten erhalten.
+        // Wenn er jedoch tries mal keine Zahl eingibt, soll eine RuntimeException ausgelöst werden mit der Meldung
+        // "Konnte keine Zahl einlesen".
+        // Hinweise: Nutze Scanner, try-catch, throw new RuntimeException
+        Scanner scanner = new Scanner(System.in);
+        while (tries > 0) {
+            tries--;
+            System.out.printf("%s: ", prompt);
+            try {
+                int number = scanner.nextInt();
+                scanner.close();
+                return number;
+            } catch (InputMismatchException e) {
+                // "Verschlucke" die fehlerhafte Eingabe, so dass die Zeichen nicht beim nächsten Eingabeversuch
+                // versehentlich gelesen werden.
+                scanner.next();
+                System.out.println("Das war keine Zahl!");
+            }
+        }
+        scanner.close();
+        throw new RuntimeException("Benutzer hat keine Zahl eingegeben");
+    }
+
+    public static int readNumberV2(String prompt, int tries) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            while (tries > 0) {
+                try {
+                    tries--;
+                    System.out.printf("%s: ", prompt);
+                    int number = scanner.nextInt();
+                    System.out.println("Jetzt wird die eingelesene Zahl an den Aufrufer zurückgegeben");
+                    return number;
+                } catch (InputMismatchException e) {
+                    System.out.println("Das war keine Zahl!");
+                    scanner.next(); // Fehlerhafte Eingabe "verschlucken"
+                }
+            }
+            throw new RuntimeException("Benutzer wollte einfach keine Zahl eingeben");
+        } finally {
+            // Der finally Block enthält Anweisungen, die IMMER ausgeführt werden. Es spielt keine Rolle, ob
+            // die Anweisungen im try-Block mit oder ohne Exception ausgeführt wurden. Demzufolge eignet sich der
+            // finally Block hervorragend für Bereinigungsaufgaben wie z.B. Ressourcenfreigaben.
+            // Selbst wenn innerhalb eines catch-Blockes eine return-Anweisung ausgeführt wird, werden die Anweisungen
+            // in finally noch kurz vorher ausgeführt.
+            // Ausführungsreihenfolge: 1) try-Block 2) ggf. ein catch-Block 3) finally-Block
+            System.out.println("Scanner wird nun geschlossen");
+            scanner.close();
+        }
+    }
+
+    // Variante, die die Fehlerbehandlung von der eigentlichen Anwendungslogik versucht möglichst gut zu trennen.
+    private static int tryReadNumber(String prompt, Scanner scanner) {
+        System.out.printf("%s: ", prompt);
+        return scanner.nextInt();
+    }
+
+    public static int readNumberV3(String prompt, int tries) {
+        // Die try-with-resources Anweisung kann man sich wie eine try-finally Anweisung vorstellen, die automatisch
+        // angeforderte Ressourcen per close-Methode schließt. Um die Ressourcen zu definieren, besitzt diese
+        // Anweisung eine Art "Ressourcenliste", die in runde Klammern eingeschlossen wird.
+        // Hinweis: Natürlich kann eine try-with-resources Anweisung auch catch-Blöcke angeben.
+        // Ausführungsreihenfolge ist: 1) try-Block 2) Automatisches Closen der Ressourcen 3) ggf. catch-Block 4) ggf. finally Block
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (tries > 0) {
+                tries--;
+                try {
+                    return tryReadNumber(prompt, scanner);
+                } catch (InputMismatchException e) {
+                    scanner.next();
+                    System.out.println("Das war keine Zahl");
+                }
+            }
+            throw new RuntimeException("Benutzer hat keine Zahl eingegeben");
+        } finally {
+            // Wenn dieser Block ausgeführt wird, sind alle Ressourcen bereits geschlossen.
+            System.out.println("Scanner wurde geschlossen");
+        }
+    }
+
+    // Aufgabe: Schreibe eine Methode, die den Nutzer Lotto spielen lässt. Der Nutzer soll 6 verschiedene Zahlen
+    // im Bereich 1-49 eingeben. Wenn der Nutzer eine falsche Eingabe macht, ist dies abzufangen und der Nutzer darauf
+    // hinzuweisen (entweder gibt Nutzer keine Zahl ein oder eine Zahl in einem ungültigen Bereich). Wenn der Nutzer
+    // dieselbe Zahl mehrfach wählt, ist dies ebenfalls ein Fehler. Zum Schluss sind alle 6 gewählten Lottozahlen
+    // in aufsteigender Reihenfolge auszugeben auf dem Terminal.
+    // Hinweis: Nur die InputMismatchException ist abzufangen! Die restlichen "Fehler" kann man durch if-Abfragen
+    // behandeln.
+
+
 
 }
