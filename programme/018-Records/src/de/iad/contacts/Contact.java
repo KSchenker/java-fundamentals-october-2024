2b817aa3027dcd0db20be7034b98a8bb39ee3b60
Max Mustermann <max.mustermann@web.de>
2024-10-22 10:35:15 +0200

    Implementierung von equals/hashCode von Contact

--- programme/018-Records/src/de/iad/contacts/Contact.java
+++ programme/018-Records/src/de/iad/contacts/Contact.java
@@ -1,8 +1,9 @@
 package de.iad.contacts;
 
 import java.util.Arrays;
+import java.util.Objects;
 
-public record Contact(String firstName, String lastName, Address address, TelephoneNumber[] phone, String mailAddress) {
+public record Contact(String firstName, String lastName, Address address, TelephoneNumber[] phones, String mailAddress) {
     @Override
     public String toString() {
         return
@@ -11,6 +12,35 @@ public record Contact(String firstName, String lastName, Address address, Teleph
             Nachname: %s
             %s
             Telefonnummern: %s
-            E-Mail: %s""".formatted(this.firstName, this.lastName, this.address, Arrays.toString(this.phone), this.mailAddress);
+            E-Mail: %s""".formatted(this.firstName, this.lastName, this.address, Arrays.toString(this.phones), this.mailAddress);
+    }
+
+    public Contact deepCopy() {
+        // Um ein Contact-Objekt vollständig zu kopieren, muss das Array der Telefonnummern dupliziert werden,
+        // da ein Array-Objekt veränderlich (mutable) ist. Alle anderen Felder sind Strings oder Records.
+        TelephoneNumber[] copyOfPhones = Arrays.copyOf(this.phones, this.phones.length);
+        return new Contact(firstName, lastName, address, copyOfPhones, mailAddress);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) return false;
+        if (this == other) return true;
+
+        if (other instanceof Contact otherContact) {
+            return this.firstName.equals(otherContact.firstName)
+                    && this.lastName.equals(otherContact.lastName)
+                    && this.address.equals(otherContact.address)
+                    && this.mailAddress.equals(otherContact.mailAddress)
+                    && Arrays.deepEquals(this.phones, otherContact.phones);
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        long phoneListHashCode = Objects.hash((Object[])this.phones);
+        return Objects.hash(firstName, lastName, address, mailAddress, phoneListHashCode);
     }
 }
--- programme/018-Records/src/de/iad/test/ContactsDemo.java
+++ programme/018-Records/src/de/iad/test/ContactsDemo.java
@@ -4,6 +4,9 @@ import de.iad.contacts.Address;
 import de.iad.contacts.Contact;
 import de.iad.contacts.TelephoneNumber;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class ContactsDemo {
     public static void main(String[] args) {
         var workPhone = new TelephoneNumber("049", "0987", "2468135");
@@ -13,13 +16,30 @@ public class ContactsDemo {
         System.out.println(address);
 
         var phoneList = new TelephoneNumber[]{workPhone, mobilePhone};
+        var phoneList2 = new TelephoneNumber[]{workPhone, mobilePhone};
 
         var contact = new Contact("Max", "Mustermann",
                 address, phoneList, "max@mustermann.de");
         System.out.println(contact);
 
         var max = new Contact("Max", "Mustermann",
-                address, phoneList, "max@mustermann.de");
-        System.out.println(contact.equals(max));
+                address, phoneList2, "max@mustermann.de");
+        System.out.println(contact.equals(max)); // true
+        System.out.println(contact.hashCode() == max.hashCode()); // true
+
+
+        Contact copyOfMax = max.deepCopy();
+        System.out.println(max.equals(copyOfMax)); // true :-)
+        System.out.println(copyOfMax.hashCode());
+        System.out.println(max.hashCode());
+
+//        Integer[] numbers = {2, 4, 6, 9};
+//        Integer[] copyOfNumbers = Arrays.copyOf(numbers, numbers.length);
+//        System.out.println(numbers == copyOfNumbers); // false (nicht dasselbe Array-Objekt)
+//        System.out.println(numbers.equals(copyOfNumbers)); // false (unerwartet)
+//        System.out.println(Arrays.deepEquals(numbers, copyOfNumbers)); // true (da die Elemente der Arrays gleich sind)
+
+
+
     }
 }
